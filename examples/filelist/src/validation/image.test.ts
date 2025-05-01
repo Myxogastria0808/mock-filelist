768d60e38e05650e5cafc40f160006cbe9bb62e6
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-01 22:09:55 +0900

    fix: repair test file to pass the test

--- examples/filelist/src/validation/image.test.ts
+++ examples/filelist/src/validation/image.test.ts
@@ -1,17 +1,14 @@
-import { describe, expect, test } from "vitest";
-import { imageSchema, ImageSchemaType } from "./image";
-import { LocalFileListBuilder } from "./sample";
+import { describe, expect, test } from 'vitest';
+import { imageSchema, ImageSchemaType } from './image';
+import { LocalFileListBuilder } from '@mock-filelist/filelist';
 
-describe("imageSchemaのバリデーション", () => {
-  test.concurrent("有効な入力", async () => {
-    const image: FileList = new LocalFileListBuilder()
-      .addFile({
-        filePath: "test_assets/sample.jpg",
-        mimeType: "image/jpeg",
-      })
+describe('imageSchemaのバリデーション', () => {
+  test.concurrent('有効な入力', async () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
       .build();
     const validInput: ImageSchemaType = {
-      image: image,
+      image: filelist,
     };
     const result = imageSchema.safeParse(validInput);
     expect(result.success).toBe(true);
