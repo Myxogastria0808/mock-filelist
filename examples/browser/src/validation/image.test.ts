13fcf334206288294af398a74e5381636a1929a1
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-02 00:13:16 +0900

    chor: update example

--- examples/browser/src/validation/image.test.ts
+++ examples/browser/src/validation/image.test.ts
@@ -1,7 +1,20 @@
 import { describe, expect, test } from 'vitest';
+import { imageSchema, ImageSchemaType } from './image';
+import { RemoteFileListBuilder } from '@mock-filelist/browser';
 
 describe('imageSchemaのバリデーション', () => {
   test.concurrent('有効な入力', async () => {
-    expect(true).toBe(true);
+    let filelist: FileList = (
+      await new RemoteFileListBuilder().addFile({
+        url: 'https://pub-5d8c638a3a5f4474b17d2a501dae7b3b.r2.dev/sample.png',
+        name: 'sample.png',
+        mimeType: 'image/png',
+      })
+    ).build();
+    const validInput: ImageSchemaType = {
+      image: filelist,
+    };
+    const result = imageSchema.safeParse(validInput);
+    expect(result.success).toBe(true);
   });
 });
