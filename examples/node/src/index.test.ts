a43ce2c5811d7b3f2ad5547115cbc9c96103c50a
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-04 15:31:07 +0900

    chor: add test

--- examples/node/src/index.test.ts
+++ examples/node/src/index.test.ts
@@ -1,7 +1,16 @@
 import { describe, expect, test } from 'vitest';
+import { imageSchema, ImageSchemaType } from './lib/schema';
+import { LocalFileArrayBuilder } from '@mock-filelist/node';
 
-describe('imageSchemaのバリデーション', () => {
-  test.concurrent('有効な入力', async () => {
-    expect(true).toBe(true);
+describe('Validation Test of imageSchema', () => {
+  test.concurrent('Vaild Value', async () => {
+    let fileArray: File[] = new LocalFileArrayBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: ImageSchemaType = {
+      image: fileArray,
+    };
+    const result = imageSchema.safeParse(validInput);
+    expect(result.success).toBe(true);
   });
 });
