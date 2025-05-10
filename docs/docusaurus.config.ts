872c160d40553ddb4fbbd6a91ebcf5852c67686c
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-11 05:05:13 +0900

    chor: update config file

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -88,30 +88,6 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/Myxogastria0808/mock-filelist/',
-            },
-          ],
-        },
-      ],
       copyright: `Copyright © ${new Date().getFullYear()} Myxogastria0808. Built with Docusaurus.`,
     },
     prism: {
